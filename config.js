e4e5345be0272945b2907bbb53e43495e53b2812
jackfire507 <jackfirepvp@Gmail.com>
2025-07-07 23:59:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "anhiuemlamquynhanhuiiii",
-    introTitle: 'Hà Quỳnh Anh',
+    introTitle: 'Hà Quỳnh Anh ưii',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -13,8 +13,8 @@ const CONFIG = {
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'em gỡ lóc anh ik, lóc thằng chó kia ik >:( cáu lémmm',
+    mess: 'anh bik mà 🥰. Anh yêu em nhiều nhiều nhiều nhiều lémmmmm 😘😘',
+    messDesc: 'em gỡ lóc anh ik, lóc thằng chó kia ik 😡 cáu lémmm',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
